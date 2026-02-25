e6bd424c092a534234584ea1646f7318eeb949e6
Veranhema <your@email.com>
2026-02-25 16:56:14 +0300

    Refactor SQL: Clean final cohort retention query  using window functions

--- sql/retention_analysis.sql
+++ sql/retention_analysis.sql
@@ -1,77 +1,11 @@
-CREATE TABLE retail_cleaned (
-    InvoiceNo VARCHAR(20),
-    StockCode VARCHAR(20),
-    Description TEXT,
-    Quantity INT,
-    InvoiceDate TIMESTAMP,
-    UnitPrice NUMERIC(10,2),
-    CustomerID INT,
-    Country VARCHAR(100),
-    TotalPrice NUMERIC(10,2),
-    InvoiceMonth VARCHAR(10),
-    CohortMonth VARCHAR(10)
-);
+/*
+Customer Retention & Cohort Analysis
+Author: Vera Nhema Kinya
+Description:
+This script calculates monthly cohort retention and retention percentage
+using PostgreSQL window functions and CTEs.
+*/
 
--- First purchase date for each customer
-SELECT 
-    customerid,
-
-    MIN(invoicedate) AS first_purchase_date
-FROM retail_cleaned
-GROUP BY customerid;
-
--- Assign cohort month
-SELECT
-    customerid,
-    DATE_TRUNC('month', MIN(invoicedate)) AS cohort_month
-FROM retail_cleaned
-GROUP BY customerid;
-
---Calculate Months Since First Purchase
-SELECT
-    r.customerid,
-    DATE_TRUNC('month', r.invoicedate) AS order_month,
-    DATE_TRUNC('month', f.first_purchase_date) AS cohort_month,
-    EXTRACT(YEAR FROM AGE(r.invoicedate, f.first_purchase_date)) * 12 +
-    EXTRACT(MONTH FROM AGE(r.invoicedate, f.first_purchase_date)) AS months_since_first
-FROM retail_cleaned r
-JOIN (
-    SELECT customerid, MIN(invoicedate) AS first_purchase_date
-    FROM retail_cleaned
-    GROUP BY customerid
-) f
-ON r.customerid = f.customerid;
-
-
---First Purchase (Cohort Month)
-SELECT
-    customerid,
-    DATE_TRUNC('month', MIN(invoicedate)) AS cohort_month
-FROM retail_cleaned
-GROUP BY customerid;
-
---Full Cohort Join
-SELECT
-    r.customerid,
-    DATE_TRUNC('month', r.invoicedate) AS invoice_month,
-    c.cohort_month,
-    
-    EXTRACT(YEAR FROM AGE(DATE_TRUNC('month', r.invoicedate), c.cohort_month)) * 12 +
-    EXTRACT(MONTH FROM AGE(DATE_TRUNC('month', r.invoicedate), c.cohort_month))
-    AS cohort_index
-
-FROM retail_cleaned r
-JOIN (
-    SELECT
-        customerid,
-        DATE_TRUNC('month', MIN(invoicedate)) AS cohort_month
-    FROM retail_cleaned
-    GROUP BY customerid
-) c
-ON r.customerid = c.customerid;
-
---Retention Table
---Convert to Retention %
 WITH cohort_data AS (
     SELECT
         r.customerid,
